6d6a0f28353393c29ec4c5825201775c73cc8c09
hvk-creator <hvk261208@proton.me>
2025-09-05 16:40:48 +0700

    Update move.sh

--- move.sh
+++ move.sh
@@ -1,2 +1,7 @@
-mv /tmp/Windows-XP.iso /root/xp.iso
-mv /tmp/xp.qcow2 /root/xp.qcow2
+if ! [ -e /root/xp.iso ] ; then
+  mv /tmp/Windows-XP.iso /root/xp.iso
+fi
+
+if ! [ -e /root/xp.qcow2 ] ; then
+  mv /tmp/xp.qcow2 /root/xp.qcow2
+fi
